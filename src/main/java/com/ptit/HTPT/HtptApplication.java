33261bee2315772e78f2c9111be5311e7697b96f
hunghv <prononstop113@gmail.com>
2023-10-31 08:56:59 +0700

    'init'

---
+++ src/main/java/com/ptit/HTPT/HtptApplication.java
@@ -0,0 +1,13 @@
+package com.ptit.HTPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtptApplication.class, args);
+	}
+
+}
